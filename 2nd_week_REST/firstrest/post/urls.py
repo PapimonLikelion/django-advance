f2f4e534e23d7b49feb6b92f29e783137d9d32f0
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-01-08 16:57:18 +0900

    feat: API View 실습

--- 2nd_week_REST/firstrest/post/urls.py
+++ 2nd_week_REST/firstrest/post/urls.py
@@ -1,12 +1,26 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from . import views
+# from django.urls import path, include
+# from rest_framework.routers import DefaultRouter
+# from . import views
 
-# REST FRAMEWORK 는 router라는 개념을 통해서 url을 결정하는구나
+# # REST FRAMEWORK 는 router라는 개념을 통해서 url을 결정하는구나
 
-router = DefaultRouter()
-router.register('post', views.PostViewSet)
+# router = DefaultRouter()
+# router.register('post', views.PostViewSet)
+
+# urlpatterns = [
+#     path('', include(router.urls)),
+# ]
+
+### API VIEW
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from post import views
+
+# Default Router는 사용하지 않는다. API ROOT가 없기때문?
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('post/', views.PostList.as_view()),
+    path('post/<int:pk>/', views.PostDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
